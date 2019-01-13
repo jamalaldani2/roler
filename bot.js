cb6722fbf2e4ef4dc8fc9b45b3ee39f0dbb6e925
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-01-13 19:35:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,31 +5,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    var prefix = "$";
-   
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**اكتب شي بعد الكوماند**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('#ff0000')
-                // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
-            });
-        }
-        } else {
-            return;
-        }
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","MEMBERS"));
     });
+
 	client.login(process.env.BOT_TOKEN);
